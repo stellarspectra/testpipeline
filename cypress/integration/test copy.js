8813070778b85904aca2fa4b63ef87d27e5a6acf
stellar spectra <paulaalmenar@hotmail.com>
2022-12-06 19:20:22 -0300

    iniciando proyecto de pipelines

---
+++ cypress/integration/test copy.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+
+describe('My First Test Suite', function() 
+{
+
+it('My FirstTest case',function() {
+
+    cy.log("Hola Mundo")
+
+}  )
+
+it('My second case',function() {
+
+    cy.visit("https://demoqa.com/")
+  
+
+}  )
+
+
+}  )
